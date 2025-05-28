697557c10c4ceb53aaabc286a73039abcc7addff
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2025-05-28 21:51:28 +0200

    fix: update dependency installation to use pnpm and add versioning script

---
+++ update-version.js
@@ -0,0 +1,10 @@
+const fs = require('node:fs');
+
+const packageVersion = fs.readFileSync('./package-version.txt', 'utf8');
+fs.removeSync('./package-version.txt');
+
+const readme = fs.readFileSync('./README.md', 'utf8');
+fs.writeFileSync('./README.md', readme.replace(/## (?:\d+\.\d+\.\d+)/, '## '+packageVersion), 'utf8');
+
+const package = fs.readFileSync('./package.json', 'utf8');
+fs.writeFileSync('./package.json', package.replace(/"version":(\s*)"(?:\d+\.\d+\.\d+)",/, '"version":$1"'+packageVersion+'",'), 'utf8');
